d45389d78a6f56f5b0ee657d247d868672d08475
vijaise <141618250+vijaise@users.noreply.github.com>
2025-05-29 20:29:21 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from langchain_community.llms import Ollama
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+#os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+#os.environ["LANGCHAIN_TRACING_V2"]="true"
+#os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
