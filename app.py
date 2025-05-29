9f82aebab0da31e23379fad4c801b4f080255b98
vijaise <141618250+vijaise@users.noreply.github.com>
2025-05-29 20:20:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_community.llms import Ollama
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+prompt=ChatPromptTemplate.from_messages(
+      [
+          ("system","you are a helpful assistant. Please response to the question asked"),
+          ("user", "Question:{question}")
+      ]
+
+)
+
+st.title("Langchain Demo with Gemma model")
+input_text=st.text_input("what question you have in mind")
+
+llm=Ollama(model ="gemma:2b")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
